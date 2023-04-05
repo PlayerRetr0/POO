cb45f9d03581861c8672372c9ba341fb8e1ba766
PlayerRetr0 <127156120+PlayerRetr0@users.noreply.github.com>
2023-04-04 21:50:46 -0300

    switch na calculadora

--- src/br/com/residencia/poo/listaum/Devertres.java
+++ src/br/com/residencia/poo/listaum/Devertres.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Devertres {
 
 	public static void main(String[] args) {
-		//Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
 		Double num1;
 		Double num2;
@@ -14,25 +14,40 @@ public class Devertres {
 		Double sub;
 		Double div;
 		Double mult;
+		int opcao;
 		System.out.println("digite o primeiro numero: ");
 		num1 = leia.nextDouble();
 		System.out.println("digite o segundo numero: ");
 		num2 = leia.nextDouble();
-		leia.close();
-		soma = num1 + num2;
-		sub = num1 - num2;
-		div = num1 * num2;
-		mult = num1 / num2;
-		System.out.println("a soma é " + soma);
-		System.out.println("a subtração é " + sub);
-		System.out.println("a multiplicação é " + mult);
-		if (num2 == 0){;
-			
-			System.out.println("não é possivel dividir por 0");
-		} else {
-			System.out.println("a divisão é " + div);
+		System.out.println("(1)soma");
+		System.out.println("(2)subtração");
+		System.out.println("(3)multiplicação");
+		System.out.println("(4)divisão");
+		switch(opcao = leia.nextInt()){
+			case 1:
+				soma = num1 + num2;
+				System.out.println("a soma é " + soma);
+				break;
+			case 2:
+				sub = num1 - num2;
+				System.out.println("a subtração é " + sub);
+				break;
+			case 3:
+				mult = num1 * num2;
+				System.out.println("a multiplicação é " + mult);
+				break;
+			case 4:
+				div = num1 / num2;
+				if (num2 == 0){;
+				
+				System.out.println("não é possivel dividir por 0");
+			} else {
+				System.out.println("a divisão é " + div);
+			}
+				break;
+				
 		}
-		
+		leia.close();
 
 	}
 
