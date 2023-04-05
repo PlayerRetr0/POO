a519c7b37b1c493d759546d7e5537e7b79185ddb
PlayerRetr0 <127156120+PlayerRetr0@users.noreply.github.com>
2023-04-05 17:52:54 -0300

    adições do get e set

--- src/br/com/residencia/poo/desafioum/BalancoTrimestral.java
+++ src/br/com/residencia/poo/desafioum/BalancoTrimestral.java
@@ -2,20 +2,23 @@ package br.com.residencia.poo.desafioum;
 
 public class BalancoTrimestral extends ContasDois {
 
-	public static void main(String[] args) {
-		BalancoTrimestral exc = new BalancoTrimestral();
-		exc.gastosFevereiro = 15000;
-		exc.gastosJaneiro = 23000;
-		exc.gastosMarco = 17000;
-		exc.gastosJunho = 37000;
-		
-		
-		
-		int gastosTrimestres = exc.gastosJaneiro + exc.gastosFevereiro + exc.gastosMarco + exc.gastosJunho;
-		System.out.println("a soma dos gastos é " +gastosTrimestres);
+	public BalancoTrimestral(int gastosJaneiro, int gastosFevereiro, int gastosMarco, int gastosJunho) {
+		super(gastosJaneiro, gastosFevereiro, gastosMarco, gastosJunho);
 
 	}
-	
 
+	public static void main(String[] args) {
+		BalancoTrimestral exc = new BalancoTrimestral(15000, 23000, 17000, 37000);
+		BalancoTrimestral exc2 = new BalancoTrimestral(12000, 13000, 15000, 27000);
+
+		int gastosTrimestres = exc.getGastosJaneiro() + exc.getGastosFevereiro() + exc.getGastosMarco() + exc.getGastosJunho();
+		System.out.printf(" janeiro: %s%n Fevereiro: %s%n março: %s%n junho: %s%n",exc.getGastosJaneiro(),exc.getGastosFevereiro(),exc.getGastosMarco(),exc.getGastosJunho());
+		System.out.println("a soma dos primeiros gastos é " + gastosTrimestres);
+		
+		int gastosTrimestres2 = exc2.getGastosJaneiro() + exc2.getGastosFevereiro() + exc2.getGastosMarco() + exc2.getGastosJunho();
+		System.out.printf(" janeiro: %s%n Fevereiro: %s%n março: %s%n junho: %s%n",exc2.getGastosJaneiro(),exc2.getGastosFevereiro(),exc2.getGastosMarco(),exc2.getGastosJunho());
+		System.out.println("a soma dos segundos gastos é " + gastosTrimestres2);
+
+	}
 
 }
--- src/br/com/residencia/poo/desafioum/ContasDois.java
+++ src/br/com/residencia/poo/desafioum/ContasDois.java
@@ -1,9 +1,48 @@
 package br.com.residencia.poo.desafioum;
 
 public class ContasDois {
-	public int gastosJaneiro;
-	public int gastosFevereiro;
-	public int gastosMarco;
-	public int gastosJunho;
+	private int gastosJaneiro;
+	private int gastosFevereiro;
+	private int gastosMarco;
+	private int gastosJunho;
+
+	public ContasDois(int gastosJaneiro, int gastosFevereiro, int gastosMarco, int gastosJunho) {
+		this.gastosJaneiro = gastosJaneiro;
+		this.gastosFevereiro = gastosFevereiro;
+		this.gastosMarco = gastosMarco;
+		this.gastosJunho = gastosJunho;
+	}
+
+	public int getGastosJaneiro() {
+		return gastosJaneiro;
+	}
+
+	public void setGastosJaneiro(int gastosJaneiro) {
+		this.gastosJaneiro = gastosJaneiro;
+	}
+
+	public int getGastosFevereiro() {
+		return gastosFevereiro;
+	}
+
+	public void setGastosFevereiro(int gastosFevereiro) {
+		this.gastosFevereiro = gastosFevereiro;
+	}
+
+	public int getGastosMarco() {
+		return gastosMarco;
+	}
+
+	public void setGastosMarco(int gastosMarco) {
+		this.gastosMarco = gastosMarco;
+	}
+
+	public int getGastosJunho() {
+		return gastosJunho;
+	}
+
+	public void setGastosJunho(int gastosJunho) {
+		this.gastosJunho = gastosJunho;
+	}
 
 }
